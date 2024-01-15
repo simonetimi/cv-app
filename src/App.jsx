a43a4c64c815d34a63d616ef5144cdeebc9282d6
Simone <150595274+simonetimi@users.noreply.github.com>
2024-01-15 13:35:48 +0100

    feat: add first form with submit button in the header, with conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,8 @@
 import './App.css';
-import { useState } from 'react';
-
 import Editor from './components/Editor';
 
-function Bridge() {
-  const [formData, setFormData] = useState({
-    header: { name: '', email: '', address: '', phone: '' },
-    education: ['school', 'city'],
-    experience: ['hi'],
-  });
-  return <Editor />;
-}
-
 function App() {
-  return <Bridge />;
+  return <Editor />;
 }
 
 export default App;
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,10 +1,54 @@
 /* eslint-disable react/prop-types */
 import '../styles/Editor.css';
+import { useState } from 'react';
+
+function Input({ label, type, id, handleOnChange }) {
+  return (
+    <label htmlFor={id}>
+      {label}:
+      <input id={id} type={type} onChange={handleOnChange} />
+    </label>
+  );
+}
 
 function Editor() {
+  const [isHeaderSubmitted, setIsHeaderSubmitted] = useState(false);
+  const [headerInput, setHeaderInput] = useState({
+    name: '',
+    bdate: '',
+    phone: '',
+    profession: '',
+    address: '',
+    email: '',
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setIsHeaderSubmitted(true);
+  }
+
+  function handleOnChange(event) {
+    const { id, value } = event.target;
+    setHeaderInput((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  }
+
   return (
     <div className='Editor'>
-      <header>hi</header>
+      {!isHeaderSubmitted ? (
+        <header>
+          <Input label='Name' type='text' id='name' handleOnChange={handleOnChange} />
+          <button type='button' onClick={handleSubmit}>
+            Submit
+          </button>
+        </header>
+      ) : (
+        <header>
+          <p>{headerInput.name}</p>
+        </header>
+      )}
     </div>
   );
 }
