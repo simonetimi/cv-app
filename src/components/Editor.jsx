7c815e0154702dad9674cccaa6284bc6f32c85bb
Simone <150595274+simonetimi@users.noreply.github.com>
2024-01-16 10:32:42 +0100

    feat: add education section

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable react/jsx-no-bind */
 /* eslint-disable react/prop-types */
 import '../styles/Editor.css';
+import '../styles/Experience.css';
+
 import Header from './Header';
+import Education from './Education';
 
 function Editor() {
   return (
     <div className='Editor'>
       <Header />
+      <Education />
     </div>
   );
 }
--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -2,4 +2,73 @@
 import { useState } from 'react';
 import Input from './Input';
 
-function Education() {}
+function CheckInput({ label, id, handleCheck }) {
+  return (
+    <label htmlFor={id}>
+      {label}:
+      <input id={id} type='checkbox' onChange={handleCheck} />
+    </label>
+  );
+}
+
+function Education() {
+  const [isEduSubmitted, setIsEduSubmitted] = useState(false);
+  const [eduInput, setEduInput] = useState([
+    {
+      program: '',
+      uni: '',
+      location: '',
+      start: '',
+      end: '',
+    },
+  ]);
+  const [eduCheck, setEduCheck] = useState(false);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setIsEduSubmitted(true);
+  }
+
+  const handleCheck = (event) => {
+    setEduCheck(event.target.checked);
+  };
+
+  const handleOnChange = (event) => {
+    const { id, value } = event.target;
+    setEduInput((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
+
+  return !isEduSubmitted ? (
+    <section className='forms'>
+      <Input label='Program' type='text' id='program' handleOnChange={handleOnChange} />
+      <Input label='University' type='text' id='uni' handleOnChange={handleOnChange} />
+      <Input label='City' type='text' id='location' handleOnChange={handleOnChange} />
+      <Input label='Enrolled in' type='date' id='start' handleOnChange={handleOnChange} />
+      {!eduCheck && (
+        <Input label='Graduated in' type='date' id='end' handleOnChange={handleOnChange} />
+      )}
+      <CheckInput label='On going' id='ongoing' handleCheck={handleCheck} />
+      <button type='button' onClick={handleSubmit}>
+        Submit
+      </button>
+    </section>
+  ) : (
+    <section className='display'>
+      <div>
+        <p>{eduInput.program}</p>
+        <p>{eduInput.uni}</p>
+      </div>
+      <div>
+        <p>
+          {eduInput.start} - {eduCheck ? 'Present' : eduInput.end}
+        </p>
+        <p>{eduInput.location}</p>
+      </div>
+    </section>
+  );
+}
+
+export default Education;
